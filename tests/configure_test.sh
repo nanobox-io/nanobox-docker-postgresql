cd2d1911be5649681f7e92cfb79423cdaec3e4b6
Daniel Barney <daniel@pagodabox.com>
2015-10-09 13:46:21 -0600

    added in UUID

--- tests/configure_test.sh
+++ tests/configure_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 . `dirname $0`/util/bash_help.sh
 
-pass "unable to start the container" docker run -d --name $0 nanobox-io/nanobox-docker-postgresql
-defer docker kill $0
+UUID=$(cat /proc/sys/kernel/random/uuid)
+pass "unable to start the container" docker run -d --name $UUID nanobox-io/nanobox-docker-postgresql
+defer docker kill $UUID
 
 # we should be able to run the configure hook
 pass "unable to run configure hook" /opt/bin/default-configure '{}'
