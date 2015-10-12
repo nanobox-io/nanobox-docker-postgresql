312569e6ffb58a3df5ff206ddf8e17be9bbf5efa
Daniel Barney <daniel@pagodabox.com>
2015-10-12 14:11:26 -0600

    added in extentions

--- tests/configure_test.sh
+++ tests/configure_test.sh
@@ -1,7 +1,7 @@
 VERSION=$1
 echo running tests for $VERSION of postgresql
 UUID=$(cat /proc/sys/kernel/random/uuid)
-PAYLOAD='{"platform":"local","boxfile":{"locale":"en_US.UTF-8"},"uid":"'$UUID'","logtap_host":"127.0.0.1"}'
+PAYLOAD='{"platform":"local","boxfile":{"extensions":{},"locale":"en_US.UTF-8"},"uid":"'$UUID'","logtap_host":"127.0.0.1"}'
 echo boxfile: "$PAYLOAD"
 
 pass "unable to start the $VERSION container" docker run --privileged=true -d --name $UUID nanobox/nanobox-docker-postgresql:$VERSION
