89aa8bbdad89830e5b341989bebddd3d50bedd37
Daniel Barney <daniel@pagodabox.com>
2015-10-12 12:27:56 -0600

    fixing variable name

--- tests/configure_test.sh
+++ tests/configure_test.sh
@@ -4,7 +4,7 @@ for VERSION in 9.3 9.4; do
   PAYLOAD='{"platform":"local","boxfile":{"locale":"en_US.UTF-8"},"uid":"'$UUID'","logtap_host":"127.0.0.1"}'
   echo boxfile: "$PAYLOAD"
 
-  pass "unable to start the $VERSION container" docker run --privileged=true -d --name $UUID94 nanobox/nanobox-docker-postgresql:$VERSION
+  pass "unable to start the $VERSION container" docker run --privileged=true -d --name $UUID nanobox/nanobox-docker-postgresql:$VERSION
   defer docker kill $UUID
 
   # we should be able to run the basic configure hook
