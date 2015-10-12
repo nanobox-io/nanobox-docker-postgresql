edfb28116bf5ef8efbf8749bbd48891359fe586f
Daniel Barney <daniel@pagodabox.com>
2015-10-12 11:13:29 -0600

    payload makes more sense

--- tests/configure_test.sh
+++ tests/configure_test.sh
@@ -3,6 +3,6 @@ pass "unable to start the container" docker run --privileged=true -d --name $UUI
 defer docker kill $UUID
 
 # we should be able to run the basic configure hook
-BOXFILE='{"platform":"local","boxfile":{"locale":"en_US.UTF-8"},"uid":"$UUID","logtap_host":"127.0.0.1"}'
-echo boxfile: "$BOXFILE"
-pass "unable to run the configure hook" docker exec $UUID /opt/bin/default-configure "$BOXFILE"
+PAYLOAD='{"platform":"local","boxfile":{"locale":"en_US.UTF-8"},"uid":"$UUID","logtap_host":"127.0.0.1"}'
+echo boxfile: "$PAYLOAD"
+pass "unable to run the configure hook" docker exec $UUID /opt/bin/default-configure "$PAYLOAD"
