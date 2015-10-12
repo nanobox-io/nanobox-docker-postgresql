9c97d0bdafcc286d51db585ac9aa9ac2f568cf01
Daniel Barney <daniel@pagodabox.com>
2015-10-12 12:02:43 -0600

    testing out multiple versions

--- tests/configure_test.sh
+++ tests/configure_test.sh
@@ -1,8 +1,12 @@
-UUID=$(cat /proc/sys/kernel/random/uuid)
-pass "unable to start the container" docker run --privileged=true -d --name $UUID nanobox-io/nanobox-docker-postgresql
-defer docker kill $UUID
+for VERSION in 9.3 9.4; do
+  echo running tests for $VERSION of postgresql
+  UUID=$(cat /proc/sys/kernel/random/uuid)
+  PAYLOAD='{"platform":"local","boxfile":{"locale":"en_US.UTF-8"},"uid":"'$UUID'","logtap_host":"127.0.0.1"}'
+  echo boxfile: "$PAYLOAD"
 
-# we should be able to run the basic configure hook
-PAYLOAD='{"platform":"local","boxfile":{"locale":"en_US.UTF-8"},"uid":"$UUID","logtap_host":"127.0.0.1"}'
-echo boxfile: "$PAYLOAD"
-pass "unable to run the configure hook" docker exec $UUID /opt/bin/default-configure "$PAYLOAD"
+  pass "unable to start the $VERSION container" docker run --privileged=true -d --name $UUID94 nanobox/nanobox-docker-postgresql:$VERSION
+  defer docker kill $UUID
+
+  # we should be able to run the basic configure hook
+  pass "unable to run the configure hook for $VERSION" docker exec $UUID /opt/bin/default-configure "$PAYLOAD"
+done
