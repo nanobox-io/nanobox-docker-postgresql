9429bb56f031891aea8c777b0b37d1985eb582a4
Braxton Huggins <braxton@pagodabox.com>
2016-02-01 10:06:31 -0700

    update payload in test, run update hook before the configure hook

--- tests/configure_test.sh
+++ tests/configure_test.sh
@@ -1,11 +1,14 @@
 VERSION=$1
 echo running tests for $VERSION of postgresql
 UUID=$(cat /proc/sys/kernel/random/uuid)
-PAYLOAD='{"platform":"local","boxfile":{"extensions":{},"locale":"en_US.UTF-8"},"uid":"'$UUID'","logtap_host":"127.0.0.1"}'
+PAYLOAD='{"boxfile":{"name":"database1","stability":"stable","version":'${VERSION}'},"logtap_host":"127.0.0.1","platform":"local","uid":"postgresql1"}'
 echo boxfile: "$PAYLOAD"
 
 pass "unable to start the $VERSION container" docker run --privileged=true -d --name $UUID nanobox/nanobox-docker-postgresql:$VERSION
 defer docker kill $UUID
+
+# we should be able to run the basic update hook
+pass "unable to run the update hook for $VERSION" docker exec $UUID /opt/bin/update "$PAYLOAD"
 
 # we should be able to run the basic configure hook
 pass "unable to run the configure hook for $VERSION" docker exec $UUID /opt/bin/default-configure "$PAYLOAD"
